41dce849fb4450347f9701ca7f169e52691b4f7e
GloomyAtaraxia <134368371+GloomyAtaraxia@users.noreply.github.com>
2023-11-09 18:52:33 +0800

    Add files via upload

---
+++ ACT6/2.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import GreetingCard from './GreetingCard';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <GreetingCard
+        title="Happy Birthday!"
+        message="Wishing you a fantastic day!"
+        buttonText="Send"
+        imageSrc="https://via.placeholder.com/300"
+      />
+      <GreetingCard
+        title="Thank You!"
+        message="Expressing our gratitude."
+        buttonText="Share"
+        imageSrc="https://via.placeholder.com/300"
+      />
+    </div>
+  );
+}
+
+export default App;
